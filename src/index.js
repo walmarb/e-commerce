24c86466684ca3c00b1b161106181d87e763b082
walmarb <walmarb@gmail.com>
2024-02-12 15:04:08 -0300

    Se rutean endpoints de productos

--- src/index.js
+++ src/index.js
@@ -1,103 +1,13 @@
-import { ProductManager } from "./config/ProductManager.js";
 import express from 'express'
+import productsRouter from './routes/products.routes.js'
 
 const PORT = 8080
 const app = express()
 
-const productManager = new ProductManager('./src/data/products.json')
-
-app.get('/', (req,res)=>{
-    res.send("Hola, desde mi primer servidor en Express")
-})
-
-app.get('/products', async (req,res) => {
-
-    try {
-
-        const { limit } = req.query
-
-        let products = await productManager.getProducts()
-        const queryLimit = parseInt(limit)
-
-        if(!isNaN(queryLimit)){
-            products = (products.slice(0, queryLimit))
-        }
-
-        res.status(200).send(products)
-
-    } catch (error) {
-        res.status(500).send(`Error interno del servidor al consultar clientes : ${ error }`)
-    }
-})
-
-app.get('/products/:pid', async (req,res) =>{
-
-    try {
-        const idProducto = req.params.pid
-        const prod = await productManager.getProductById(idProducto)
-        if(prod){
-            res.status(200).send(prod)
-        }else{
-            res.status(404).send("Producto no existe")
-        }
-    } catch (error) {
-        res.status(500).send("Error interno al buscar un producto particular")
-    }
-
-})
-
-app.post('/products', async (req,res) => {
-
-    console.log(req.body)
-    try {
-        const product = req.body
-        console.log(product,"llego")
-        const message = await productManager.addProduct(product)
-        console.log(message, "aca")
-        if(message == 'Producto creado correctamente'){
-            res.status(200).send(message)
-        }else{
-            res.status(400).send(message)
-        }
-    } catch (error) {
-        res.status(500).send(`Error al crear producto : ${error}`)
-    }
-})
-
-app.put('/products/:pid', async (req,res) => {
-    try {
-        const idProducto = req.params.pid
-        const product = req.body
-        const message = await productManager.updateProduct(idProducto, product)
-
-        if(message == "Producto actualizado correctamente"){
-            res.status(200).send(message)
-        }else{
-            res.status(404).send(message)
-        }
-    } catch (error) {
-        res.status(500).send(`Error al modificar producto : ${error}`)
-    }
-})
-
-app.delete('/products/:pid', async (req,res) => {
-    try {
-        const idProducto = req.params.pid
-        const message = await productManager.deleteProduct(idProducto)
-
-        if(message == "Producto eliminado correctamente"){
-            res.status(200).send(message)
-        }else{
-            res.status(404).send(message)
-        }
-    } catch (error) {
-        res.status(500).send(`Error al eliminar producto : ${error}`)
-    }
-})
-
+app.use(express.json())
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
 })
 
-app.use(express.json())
+app.use('/products', productsRouter)
