c67ff38272721c13d4365853040846c2e63b2c80
walmarb <walmarb@gmail.com>
2024-02-19 17:28:44 -0300

    Se agrega public y se corrige naming de endpoints

--- src/index.js
+++ src/index.js
@@ -1,16 +1,17 @@
 import express from 'express'
 import productsRouter from './routes/products.routes.js'
 import cartsRouter from './routes/carts.routes.js'
+import {__dirname} from './path.js'
 
 const PORT = 8080
 const app = express()
 
 app.use(express.json())
+app.use('/static',express.static(__dirname + '/public'))
+
+app.use('/api/products', productsRouter)
+app.use('/api/carts', cartsRouter)
 
 app.listen(PORT, () => {
     console.log(`Server on port ${PORT}`)
 })
-
-app.use('/products', productsRouter)
-
-app.use('/carts', cartsRouter)
